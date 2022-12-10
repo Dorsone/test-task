5f602c4aea52843326ea6fb52dd7c1de573ffc5a
Dorsone <jasur.coder@gmail.com>
2022-12-10 13:36:37 +0500

    profile update

--- app/Http/Requests/ProfileUpdateRequest.php
+++ app/Http/Requests/ProfileUpdateRequest.php
@@ -17,7 +17,7 @@ class ProfileUpdateRequest extends FormRequest
     {
         return [
             'name' => ['string', 'max:255'],
-            'email' => ['email', 'max:255', Rule::unique(User::class)->ignore($this->user()->id)],
+            'login' => ['string', 'max:255', Rule::unique(User::class, 'login')->ignore($this->user()->id)],
         ];
     }
 }
